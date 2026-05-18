32b81b050e739f7d16852cec0a718bc0d5a1f712
GiorgiEZUGBA <gezug24@freeuni.edu.ge>
2026-05-18 13:03:12 +0400

    created project structure

---
+++ server/src/main/java/RoadReport/RoadReportApplication.java
@@ -0,0 +1,13 @@
+package RoadReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadReportApplication.class, args);
+	}
+
+}
