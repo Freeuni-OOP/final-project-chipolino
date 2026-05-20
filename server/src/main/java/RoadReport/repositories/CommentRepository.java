2b6978c98481f496496c44afb0b0ce3122fe1a36
Nika <nshub24@freeuni.edu.ge>
2026-05-20 17:44:21 +0400

    Implemented DAO class for comment

--- server/src/main/java/RoadReport/repositories/CommentRepository.java
+++ server/src/main/java/RoadReport/repositories/CommentRepository.java
@@ -1,10 +1,20 @@
 package RoadReport.repositories;
 
 import RoadReport.entities.Comment;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByReportId(Long reportId);
 
+    List<Comment> findByUserId(Long userId);
+
+    void deleteByReportId(Long reportId);
+
+    long countByReportId(Long reportId);
 }
