2314bfcc6fbcffccce40279cf1e43791d77b7c2b
nikoloz-bendianishvili <nbend24@freeuni.edu.ge>
2026-05-22 14:58:26 +0400

    added JwtService and ReportCleanupService, resolved merge conflicts

--- server/src/main/java/RoadReport/services/cleanUp/ReportCleanupService.java
+++ server/src/main/java/RoadReport/services/cleanUp/ReportCleanupService.java
@@ -1,7 +1,23 @@
 package RoadReport.services.cleanUp;
 
+import RoadReport.repositories.ReportRepository;
+import jakarta.transaction.Transactional;
+import lombok.Data;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Data
 @Service
+@EnableScheduling
 public class ReportCleanupService {
+
+    private final ReportRepository reportCleanupRepository;
+
+
+    @Scheduled(fixedDelay = 10000)
+    @Transactional
+    public void cleanupReports() {
+        reportCleanupRepository.deleteExpiredReports();
+    }
 }
