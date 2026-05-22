d647c9090dd975151a613f0147467d1527ef562b
Nika <nshub24@freeuni.edu.ge>
2026-05-22 16:34:18 +0400

    implemented comment service tests using mocking

--- server/src/test/java/RoadReport/TestServices/TestCore/TestUserService.java
+++ server/src/test/java/RoadReport/TestServices/TestCore/TestUserService.java
@@ -1,4 +1,24 @@
 package RoadReport.TestServices.TestCore;
 
+import RoadReport.entities.Comment;
+import RoadReport.entities.Report;
+import RoadReport.entities.User;
+import RoadReport.enums.ReportStatus;
+import RoadReport.enums.ReportType;
+import RoadReport.enums.Role;
+import RoadReport.repositories.CommentRepository;
+import RoadReport.services.core.CommentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class TestUserService {
+
 }
