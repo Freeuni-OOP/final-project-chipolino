8dfb64e057cce26b6f267e280f59b09e37c3ece0
GiorgiEZUGBA <gezug24@freeuni.edu.ge>
2026-06-07 19:53:08 +0400

    added java-doc documentations

--- server/src/main/java/RoadReport/controllers/UserController.java
+++ server/src/main/java/RoadReport/controllers/UserController.java
@@ -17,6 +17,12 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    /**
+     * Gets full profile information of the authenticated user.
+     * Endpoint: {@code GET /api/users/me}
+     * @param userDetails the details of current user,
+     * @return {@link SelfResponseDTO} with extended profile data
+     */
     @GetMapping("/me")
     public SelfResponseDTO getCurrentUser
             (@AuthenticationPrincipal RoadUserDetails userDetails){
@@ -33,6 +39,13 @@ public class UserController {
         );
     }
 
+
+    /**
+     * Gets profile information of a user by its unique identifier.
+     * Endpoint: {@code GET /api/users/{id}}
+     * @param id the unique identifier of the requested user
+     * @return {@link UserResponseDTO} with public details only
+     */
     @GetMapping("/{id}")
     public UserResponseDTO getUser(@PathVariable Long id){
         User user = userService.getUserById(id);
@@ -45,6 +58,13 @@ public class UserController {
         );
     }
 
+    /**
+     * Updates personal details of the authenticated user.
+     * Endpoint: {@code PUT /api/users/me}
+     * @param userDetails the details of current user
+     * @param updateData  the DTO containing the new user profile data
+     * @return {@link SelfResponseDTO} with extended updated profile data
+     */
     @PutMapping("/me")
     public SelfResponseDTO updateUser
             (@AuthenticationPrincipal RoadUserDetails userDetails,
@@ -62,6 +82,12 @@ public class UserController {
         );
     }
 
+
+    /**
+     * Deletes the account of the currently authenticated user from the system.
+     * Endpoint: {@code DELETE /api/users/me}
+     * @param userDetails the details of current user account being deleted
+     */
     @DeleteMapping("/me")
     public void deleteCurrentUser
             (@AuthenticationPrincipal RoadUserDetails userDetails){
